9a0811d50d9bed7dd17c6cd4ffca1ad3b2b82aec
David Tittle <drtittle@us.ibm.com>
2016-09-20 18:11:18 -0500

    Testing express again.

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// DT var io = require('socket.io')(server);
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
@@ -25,4 +27,42 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url + ":" + appEnv.port);
+});
+
+
+// DT
+require('./node_modules/rtcmulticonnection-v3/Signaling-Server.js')(app, function(socket)
+{
+    try
+    {
+        var params = socket.handshake.query;
+
+        // "socket" object is totally in your own hands!
+        // do whatever you want!
+
+        // in your HTML page, you can access socket as following:
+        // connection.socketCustomEvent = 'custom-message';
+        // var socket = connection.getSocket();
+        // socket.emit(connection.socketCustomEvent, { test: true });
+
+        if (!params.socketCustomEvent)
+        {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function(message)
+        {
+            try
+            {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            }
+            catch (e)
+            {
+            	
+            }
+        });
+    }
+    catch (e)
+    {
+    }
 });
