cb3f25a67246158646baf2ecb036b2b65f5fcd4a
David Tittle <drtittle@us.ibm.com>
2016-09-20 16:05:28 -0500

    Working on serverHandler

--- appRTC.js
+++ appRTC.js
@@ -35,6 +35,8 @@ var server = require(isUseHTTPs ? 'https' : 'http'),
     path = require('path'),
     fs = require('fs');
 
+
+/*
 function serverHandler(request, response)
 {
     var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
@@ -45,6 +47,89 @@ function serverHandler(request, response)
     response.write('Hi there from a Custom Bluemix RTC Server over Node.js and Socket.io' + path.join('/', uri) + '\n');
     response.end();
     return;
+}
+*/
+
+
+function serverHandler(request, response)
+{
+    var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
+    var stats;
+
+    try
+    {
+        stats = fs.lstatSync(filename);
+    }
+    catch (e)
+    {
+        response.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+        response.end();
+        return;
+    }
+
+    if (fs.statSync(filename).isDirectory())
+    {
+        response.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+
+        if (filename.indexOf('/demos/MultiRTC/') !== -1)
+        {
+            filename = filename.replace('/demos/MultiRTC/', '');
+            filename += '/demos/MultiRTC/index.html';
+        }
+        else if (filename.indexOf('/demos/') !== -1)
+        {
+            filename = filename.replace('/demos/', '');
+            filename += '/demos/index.html';
+        }
+        else
+        {
+            filename += '/demos/index.html';
+        }
+    }
+
+
+    fs.readFile(filename, 'utf8', function(err, file)
+    {
+        if (err)
+        {
+            response.writeHead(500, {
+                'Content-Type': 'text/plain'
+            });
+            response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+            response.end();
+            return;
+        }
+
+        try
+        {
+            var demos = (fs.readdirSync('demos') || []);
+            if (demos.length)
+            {
+                var h2 = '<h2 style="text-align:center;display:block;"><a href="https://www.npmjs.com/package/rtcmulticonnection-v3"><img src="https://img.shields.io/npm/v/rtcmulticonnection-v3.svg"></a><a href="https://www.npmjs.com/package/rtcmulticonnection-v3"><img src="https://img.shields.io/npm/dm/rtcmulticonnection-v3.svg"></a><a href="https://travis-ci.org/muaz-khan/RTCMultiConnection"><img src="https://travis-ci.org/muaz-khan/RTCMultiConnection.png?branch=master"></a></h2>';
+                var otherDemos = '<section class="experiment" id="demos"><details><summary style="text-align:center;">Check ' + (demos.length - 1) + ' other RTCMultiConnection-v3 demos</summary>' + h2 + '<ol>';
+                demos.forEach(function(f) {
+                    if (f && f !== 'index.html' && f.indexOf('.html') !== -1) {
+                        otherDemos += '<li><a href="/demos/' + f + '">' + f + '</a> (<a href="https://github.com/muaz-khan/RTCMultiConnection/tree/master/demos/' + f + '">Source</a>)</li>';
+                    }
+                });
+                otherDemos += '<ol></details></section><section class="experiment own-widgets latest-commits">';
+
+                file = file.replace('<section class="experiment own-widgets latest-commits">', otherDemos);
+            }
+        }
+        catch (e) 
+        {}
+
+
+        response.writeHead(200);
+        response.write(file, 'utf8');
+        response.end();
+    });
 }
 
 var app = server.createServer(serverHandler);
